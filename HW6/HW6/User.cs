20205f2eba26957c7cc9da70b88dc9850b2f68dd
Аслан Нурсеитов <grooves97@gmail.com>
2019-04-25 11:47:39 +0600

    Создал классы. Проблема добавления элемента Listbox

---
+++ HW6/HW6/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
